5d68e0d317d128b308b8592a8aee33e7160ba3a9
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-14 20:15:50 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// 스와퍼 슬라이드
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+

8187bcf22dc839da6ad3ac996b15ec66ddb38da0
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-17 15:27:11 +0900

    월요일 열심히 하자!

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
 // 스와퍼 슬라이드
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
-
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+ 
+  });

74fdffbb3821f5dc8d5af0b034c3a0511fb35a0f
윤성준 <75359085+ytw418@users.noreply.github.com>
2021-05-21 18:40:51 +0900

    DD

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+  },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
